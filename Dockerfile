b697810c560fb91663bc9ceba6191c7e4cec76ff
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2017-06-06 17:56:40 +0200

    Added REQUESTS_CA_BUNDLE to allow connect to kubernetes api selfsigned

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 FROM       python:latest
 MAINTAINER Samuel Terburg <sterburg@redhat.com>
 USER       0
+VOLUME     /etc/letsencrypt \
+           /var/log/letsencrypt \
+           /var/lib/letsencrypt
+WORKDIR    /var/lib/letsencrypt
 
-ENV        APP_SCRIPT=/usr/local/bin/watch.sh
+ENV        APP_SCRIPT=/usr/local/bin/watch.sh \
+           REQUESTS_CA_BUNDLE=/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
 ENTRYPOINT /usr/libexec/s2i/run
 
 RUN  source /opt/rh/rh-python35/enable && \
--- control.sh
+++ control.sh
@@ -8,12 +8,19 @@ PROJECT=$1
 ROUTE=$2
 DOMAIN=$3
 TLS=$4
+EMAIL=${EMAIL:-sterburg@hoolia.eu}
 
 echo "$PHASE: Project '$PROJECT' with Route '$ROUTE' and Domain '$DOMAIN' has TLS '$TLS'"
 
 case $PHASE in
 Added | Updated | Sync )
   certbot run -d $DOMAIN \
+          --config-dir=/etc/letscencrypt \
+          --work-dir=/var/lib/letscencrypt \
+          --logs-dir=/var/log/letscencrypt \
+          --non-interactive \
+          -m $EMAIL \
+          --agree-tos \
           -i certbot-openshift:installer \
           --certbot-openshift:installer-api-host kubernetes.default \
           --certbot-openshift:installer-namespace $PROJECT \
