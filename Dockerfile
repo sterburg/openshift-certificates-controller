d4b7ca2634c23ebcffa632f44a3b94a30d8f49f6
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2017-06-07 11:33:06 +0200

    Added certbot authenticator 'standalone' by temp replacing route object

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM       python:latest
 MAINTAINER Samuel Terburg <sterburg@redhat.com>
 USER       0
+EXPOSE     8080 8443
 VOLUME     /etc/letsencrypt \
            /var/log/letsencrypt \
            /var/lib/letsencrypt
 WORKDIR    /var/lib/letsencrypt
 
-ENV        APP_SCRIPT=/usr/local/bin/watch.sh \
-           REQUESTS_CA_BUNDLE=/run/secrets/kubernetes.io/serviceaccount/ca.crt
+ENV        APP_SCRIPT=/usr/local/bin/watch.sh
 
 ENTRYPOINT /usr/libexec/s2i/run
 
--- control.sh
+++ control.sh
@@ -2,6 +2,9 @@
 
 . /opt/rh/rh-python35/enable
 
+cat /etc/pki/tls/certs/ca-bundle.trust.crt /run/secrets/kubernetes.io/serviceaccount/ca.crt >/tmp/ca-bundle.crt
+export REQUESTS_CA_BUNDLE=/tmp/ca-bundle.crt
+
 TOKEN=`cat /run/secrets/kubernetes.io/serviceaccount/token`
 PHASE=${PHASE:-Added}
 PROJECT=$1
@@ -14,6 +17,12 @@ echo "$PHASE: Project '$PROJECT' with Route '$ROUTE' and Domain '$DOMAIN' has TL
 
 case $PHASE in
 Added | Updated | Sync )
+  ## replace route with temp route to certbot
+  oc -n $PROJECT export route/$ROUTE >/tmp/route.old.yaml
+  oc expose pod/$HOSTNAME
+  oc expose svc/$HOSTNAME --hostname=$DOMAIN
+
+  ## request certificate
   certbot run -d $DOMAIN \
           --config-dir=/etc/letscencrypt \
           --work-dir=/var/lib/letscencrypt \
@@ -21,10 +30,18 @@ Added | Updated | Sync )
           --non-interactive \
           -m $EMAIL \
           --agree-tos \
+          -a standalone \
+          --tls-sni-01-port 8443 \
+          --http-01-port 8080 \
+          --preferred-challenges http \
           -i certbot-openshift:installer \
           --certbot-openshift:installer-api-host kubernetes.default \
           --certbot-openshift:installer-namespace $PROJECT \
           --certbot-openshift:installer-token $TOKEN
+
+   ## restore original 'route'
+   oc delete route/$HOSTNAME svc/$HOSTNAME
+   oc -n $PROJECT create -f /tmp/route.old.yaml
   ;;
 Deleted )
   echo "Removing Route, but Removing of certificate is not implemented yet."
